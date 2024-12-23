1d0c4dce4e8f03c2735d0e78250b7089b96ac60a
ian-hui <ianhuiii@sjtu.edu.cn>
2024-12-23 20:36:32 +0800

    feat: renew timeout timestamp when restart opsrequest
    Signed-off-by: ian-hui <ianhuiii@sjtu.edu.cn>

--- pkg/constant/annotations.go
+++ pkg/constant/annotations.go
@@ -57,6 +57,11 @@ const (
 	MultiClusterServicePlacementKey = "apps.kubeblocks.io/multi-cluster-service-placement"
 )
 
+// annotation for renew the opsrequest
+const (
+	OpsRequestRestartTimestampAnnotationKey = "apps.kubeblocks.io/opsrequest-restart-timestamp"
+)
+
 func InheritedAnnotations() []string {
 	return []string{
 		RestoreFromBackupAnnotationKey,
